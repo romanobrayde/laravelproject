48a2d2b88f3278aa754f7736319e9475b9ebbf7b
romanobrayde <bcaamal1601@gmail.com>
2023-10-07 00:07:45 -0600

    PWA Progresiva

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,16 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=10, user-scalble=no">
+        <meta name="theme-color" content="#FFFFFF">
+        <meta name="mobileOptimized" content="width">
+        <meta name="handleFriendly" content="true">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+        <link rel="manifest" href="{{asset('manifest.json')}}">
+        <script type="text/javascript" src="{{asset('main.js')}}"></script>
+
         <title>Laravel</title>
 
         <!-- Fonts -->
